18c24e247362264be413ad524efd21012c8393ff
Richard Newman <Richard Newman  rnewman@franz.com>
2009-08-05 13:53:47 -0700

    Ensure that MD5s are padded with zeroes to 32 chars wide.

--- src/uk/co/holygoat/util/md5.clj
+++ src/uk/co/holygoat/util/md5.clj
@@ -6,6 +6,25 @@
        MessageDigest)
      (java.math BigInteger)))
 
+;; Supposedly efficient padding routine.
+(defmacro pad [pad-to pad-char #^String s]
+  `(let [ss# ~s
+         len# (int (.length ss#))
+         diff# (int (- len# ~pad-to))
+         #^String padstring# ~(apply str (repeat pad-to pad-char))]
+     
+     (if (< diff# 0)
+       ;; Needs padding.
+       (.concat (.substring padstring# (+ ~pad-to diff#))
+                 ss#)
+       ;; Either 0 (happy) or not (oh dear). In the failure case there's
+       ;; nothing we can do...
+       ss#)))
+
+
+;; Might want to replace this with Apache Commons Codec DigestUtil:
+;; http://commons.apache.org/codec/api-release/org/apache/commons/codec/digest/DigestUtils.html
+;; String md5 = DigestUtils.md5Hex(str);
 (defn md5-sum
   "Compute the hex MD5 sum of a string."
   [#^String str]
@@ -13,6 +32,7 @@
               (.reset)
               (.update (.getBytes str)))]
     (try
-      (.toString (new BigInteger 1 (.digest alg)) 16)
+      (pad 32 \0
+        (.toString (new BigInteger 1 (.digest alg)) 16))
       (catch NoSuchAlgorithmException e
         (throw (new RuntimeException e))))))
