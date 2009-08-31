863f290ffb9fdd8c625dd36423a50bce667fdbd6
Richard Newman <rnewman@twinql.com>
2009-08-30 18:27:48 -0700

    Add test framework, tests for MD5.

---
+++ tests/test_facebook.clj
@@ -0,0 +1,33 @@
+(ns test-facebook
+  (:require [clojure.test :as t])
+  (:gen-class))
+
+;; Shamefully stolen from the Clojure tests.
+
+(def test-namespaces
+  ['uk.co.holygoat.util.md5-test])
+
+(defn run []
+  (println "Loading tests...")
+  (apply require :reload-all test-namespaces)
+  (apply t/run-tests test-namespaces))
+ 
+(defn run-ant []
+  (let [rpt t/report]
+    (binding [;; binding to *err* because, in ant, when the test target
+              ;; runs after compile-clojure, *out* doesn't print anything
+              *out* *err*
+              t/*test-out* *err*
+              t/report (fn report [m]
+                         (if (= :summary (:type m))
+                           (do (rpt m)
+                               (if (or (pos? (:fail m)) (pos? (:error m)))
+                                 (throw (new Exception (str (:fail m) " failures, " (:error m) " errors.")))))
+                           (rpt m)))]
+      (run))))
+ 
+(defn -main
+  "Run all defined tests from the command line"
+  [& args]
+  (run)
+  (System/exit 0))
