ff613a8a4910494008d448d441fc85204779d495
Richard Newman <rnewman@twinql.com>
2009-10-18 13:20:03 -0700

    Add a stub Facebook API server which simply echoes its parameters as JSON. Use this to write a test which verifies that optional parameters make it to the server. Demonstrates the use of an optional parameter.

--- tests/com/twinql/clojure/facebook/compojure_test.clj
+++ tests/com/twinql/clojure/facebook/compojure_test.clj
@@ -1,12 +1,23 @@
 (ns com.twinql.clojure.facebook.compojure-test
   (:refer-clojure)
   (:require compojure)
+  (:require [org.danlarkin.json :as json])
   (:require [com.twinql.clojure.http :as http])
   (:require [compojure.server.grizzly :as grizzly])
   (:use clojure.test
         com.twinql.clojure.facebook))
         
 (compojure/defroutes compojure-test-app
+                    
+  ;; Let's make a fake API listener and see what we're sending.
+  (compojure/POST "/restserver.php"
+    (with-fb-keys ["323ba847e1fc870fd7ee26e5d23ae100"
+                   "037e6278b8bf2c317b5c5a789c736f2d"]
+      {:status 200
+       :headers {"Content-Type" "text/json"}
+       :body (json/encode-to-str params)}))
+                    
+  ;; Test post-auth callbacks.
   (compojure/ANY "/test/postauth"
     (with-fb-keys ["323ba847e1fc870fd7ee26e5d23ae100"
                    "037e6278b8bf2c317b5c5a789c736f2d"]
@@ -21,11 +32,32 @@
             "No parameters."))
         "Invalid signature."))))
 
+;; Abstract away the task of starting a local server, stopping it 
+;; when we're done.
+(defmacro with-local-servlet [[grizzly-args test-app] & body]
+  `(let [server# (grizzly/grizzly-server
+                   ~grizzly-args
+                   "/*" (compojure/servlet ~test-app))]
+     (.start server#)
+     (try
+       (do
+         ~@body)
+       (finally
+         (.stop server#)))))
+
+(deftest basic-api-call
+  (with-local-servlet [{:port 9123} compojure-test-app]
+    (binding [com.twinql.clojure.facebook.request/*facebook-rest-api*
+              "http://127.0.0.1:9123/restserver.php"]
+      (with-fb-keys ["323ba847e1fc870fd7ee26e5d23ae100"
+                     "037e6278b8bf2c317b5c5a789c736f2d"]
+        (with-new-fb-session []
+          (let [result (admin-get-banned-users :uids [123 456])]
+            (is (= (json/decode-from-str (:uids result))
+                   [123 456]))))))))
+    
 (deftest compojure-post-auth-callback
-  (let [server (grizzly/grizzly-server
-                 {:port 9123}
-                 "/*" (compojure/servlet compojure-test-app))]
-    (.start server)
+  (with-local-servlet [{:port 9123} compojure-test-app]
     (let [result
           (try
             (.trim
@@ -49,5 +81,4 @@
                   :as :string)))
             (catch Throwable e
               (report :fail (str "Got exception " e " making post-auth request."))))]
-      (.stop server)
       (is (= result "Success")))))
