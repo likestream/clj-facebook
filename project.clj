a05e877861e8cbfc24c4f8aff9240ad168b40293
Richard Newman <rnewman@twinql.com>
2010-02-06 17:53:44 -0800

    Add project.clj.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.twinql.clojure/facebook "1.0.0-master-SNAPSHOT"
+ :description "Clojure Facebook client library using clj-apache-http."
+ :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
+                [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
+                [org.danlarkin/clojure-json "1.1-SNAPSHOT-clojure110"] 
+                [com.twinql.clojure/clj-apache-http "1.1.0"]]
+ :test-dependencies [[compojure "0.3.2"]])
