53e90e20b8fa1a349998f14fa4e3298a5ffe6313
Richard Newman <rnewman@twinql.com>
2010-03-18 13:24:55 -0700

    Use upstream clojure-json.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject com.twinql.clojure/facebook "1.0.2-master-SNAPSHOT"
+(defproject com.twinql.clojure/facebook "1.0.5-master-SNAPSHOT"
  :description "Clojure Facebook client library using clj-apache-http."
  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
-                [org.danlarkin/clojure-json "1.1-SNAPSHOT-clojure110"] 
+                [org.danlarkin/clojure-json "1.1-SNAPSHOT"]
+                ; [org.clojars.rnewman/clojure-json "1.2.2-SNAPSHOT-clojure120"] 
                 [com.twinql.clojure/clj-apache-http "2.0.0"]]
  :test-dependencies [[compojure "0.3.2"]])
